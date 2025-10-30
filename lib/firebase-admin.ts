7aaab78971a1fe48648f1deda2d6e6b600601aa7
farislr <LRFaris24@gmail.com>
2025-10-31 05:57:54 +0700

    fix: actions build script
    fix: docker compose get from image
    
    feat: add deployment script
    
    fix: update env deploy script
    
    add arm64 platform

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,11 +1,19 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
+function getFirebaseAdminPrivateKey() {
+  const privateKeyBase64 = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY;
+
+  const pk = Buffer.from(privateKeyBase64 || '', 'base64').toString('utf-8');
+
+  return pk
+}
+
 const firebaseAdminConfig = {
   credential: cert({
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: getFirebaseAdminPrivateKey(),
   }),
 };
 
